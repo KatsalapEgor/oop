53d9cb63e44416b85f228f110d2aef8f6d9a2ceb
KatsalapEgor <y.b.katsalap@student.khai.edu>
2021-10-02 15:13:47 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java");
+=======
+        System.out.println("education");
+>>>>>>> 01b1955 (test.java added to feaure_branch)
     }
 }
