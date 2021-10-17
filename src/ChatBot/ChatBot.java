d0f76c5c90483e50e63cbfeedabfe4ab486374ea
KatsalapEgor <y.b.katsalap@student.khai.edu>
2021-10-17 15:04:09 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,18 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Obema.");
+        System.out.println("I was created in 2021.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+        System.out.println("What a greate name you have, " + name + "!");
+
+
+    }
 }
